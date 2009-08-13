840ab0c53f517ca25ed225eed84b2eac61b3708c
justin@basho.com <none@none>
2009-08-13 11:06:58 -0400

    debugging startup script

---
+++ debug-restart.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# debug-restart <configfile>
+# assuming we've run a node from here before, start back up in debug mode
+NODENAME=$(erl -noshell -pa ebin -eval "error_logger:tty(false), riak_app:read_config(\"$1\"), io:format(\"~p~n\",[riak:get_app_env(riak_nodename)])" -run init stop)
+RHOSTNAME=$(erl -noshell -pa ebin -eval "error_logger:tty(false), riak_app:read_config(\"$1\"), io:format(\"~s~n\",[riak:get_app_env(riak_hostname)])" -run init stop)
+if [ "$NODENAME" = "no_riak_nodename_undefined" ]; then
+    echo "riak_nodename not set in config file, cannot start";
+else
+    exec erl -connect_all false -pa deps/*/ebin -pa ebin -name ${NODENAME}@${RHOSTNAME} -run riak start $1 -run riak_startup rejoin
+fi
