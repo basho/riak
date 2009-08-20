a169bf17ae65c5aa85811e54ba7128765de83bc2
Ryan Tilder <rtilder@basho.com>
2009-08-20 08:41:40 -0700

    I keep starting start-join incorrectly and don't realize it for a while.

--- start-backup-restore.sh
+++ start-backup-restore.sh
@@ -4,6 +4,12 @@
 #  Join riak cluster <clustername> using erlcookie <cookie>
 #  via the node listening at <ip>:<port>
 #  and overwrite cluster data with that contained in <dumpfilename>
+if [ $# -le 5 ]; then
+    echo Usage: 1>&2
+    echo "    `basename $0` <clustername> <cookie> <ip in ring> " 1>&2
+    echo "                  <doorbell port> <filename to restore from>" 1>&2
+    exit 1
+fi
 . riak-env.sh
 erl -noshell -pa deps/*/ebin -pa ebin -name backup_restore -run riak_backup restore_config $1 $2 -run riak start -run riak_backup do_restore $3 $4 $2 $5 -run init stop
 
--- start-eventer.sh
+++ start-eventer.sh
@@ -4,5 +4,12 @@
 #  Join riak cluster <clustername> using erlcookie <cookie>
 #  via the node listening at <ip>:<port>
 #  and register a riak event handler
+if [ $# -le 7 ]; then
+    echo Usage: 1>&2
+    echo "    `basename $0` <clustername> <cookie> <ip in ring> " 1>&2
+    echo "                  <doorbell port> <nodename> <event module name>" 1>&2
+    echo "                  <event module args>" 1>&2
+    exit 1
+fi
 . riak-env.sh
 erl -noshell -pa deps/*/ebin -pa ebin -name $5 -run riak_eventer eventer_config $1 $2 -run riak start -run riak_eventer do_eventer $3 $4 $6 $7
