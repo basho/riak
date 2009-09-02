66787c5eeaa19c7faf03b0bb6ad0d55159aa8a1e
Bryan Fink <bryan@basho.com>
2009-09-02 14:11:47 -0400

    cover other paths through reduce_plist_sum

--- src/riak_mapreduce.erl
+++ src/riak_mapreduce.erl
@@ -157,7 +157,7 @@ reduce_sum_test() ->
 reduce_plist_sum_test() ->
     PLs = [[{a, 1}], [{a, 2}],
            [{b, 1}], [{b, 4}]],
-    [{a,3},{b,5}] = reduce_plist_sum(PLs, test).
-
-
+    [{a,3},{b,5}] = reduce_plist_sum(PLs, test),
+    [{a,3},{b,5}] = reduce_plist_sum(lists:flatten(PLs), test),
+    [] = reduce_plist_sum([], test).
 
