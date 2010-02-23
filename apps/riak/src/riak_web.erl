dd8645408197ac116626516e3e1cf6f71f2dc498
Andy Gross <andy@basho.com>
2010-02-23 10:43:15 -0500

    wearin' the wolf shirt

--- apps/riak/src/riak_web.erl
+++ apps/riak/src/riak_web.erl
@@ -53,7 +53,7 @@ dispatch_table() ->
         mapred_resource, MapredProps},
        {[proplists:get_value(prefix, StatsProps)],
         stats_http_resource, StatsProps},
-       {["ping"], ping_http_resource, []}]),
+       {["ping"], ping_http_resource, []}]).
 
 raw_dispatch() ->
     case riak:get_app_env(raw_name) of
