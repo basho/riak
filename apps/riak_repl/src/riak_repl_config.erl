604670aa05c76b6837e7e81d902fe4950f3e9820
Andy Gross <andy@basho.com>
2010-05-04 22:24:01 -0400

    implement some riak-repl commands

--- apps/riak_repl/src/riak_repl_config.erl
+++ apps/riak_repl/src/riak_repl_config.erl
@@ -3,7 +3,7 @@
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
--export([set_ring/1, add_local_listener/3]).
+-export([set_ring/1, add_listener/1, add_listener/3]).
 -record(state, {ring}).
 
 
@@ -14,7 +14,11 @@ init([]) -> {ok, do_initialize(#state{})}.
 
 set_ring(Ring) -> gen_server:cast(?MODULE, {set_ring, Ring}).
 
-add_local_listener(NodeName, ListenIP, Port) ->
+
+add_listener([NodeName, ListenIP, Port]) ->
+    add_listener(list_to_atom(NodeName), ListenIP, list_to_integer(Port)).
+
+add_listener(NodeName, ListenIP, Port) ->
     gen_server:call(?MODULE, {add_local_listener, NodeName, ListenIP, Port}).
 
 handle_call({add_local_listener, NodeName, ListenIP, Port}, _From, State) -> 
@@ -42,20 +46,24 @@ handle_add_local_listener(Node, ListenIP, Port, State=#state{ring=Ring}) ->
         case proplists:get_value(Node, LocalListeners) of
             undefined ->
                 [{Node, {ListenIP, Port}}|LocalListeners];
-            Listeners ->
-                [{Node, {ListenIP, Port}}|proplists:delete(Node, Listeners)]
+            _ ->
+                [{Node, {ListenIP,Port}}|proplists:delete(Node, LocalListeners)]
         end,
     NewRing = case NewListeners =:= LocalListeners of
         true -> 
             Ring;
-        false -> riak_core_ring_manager:set_my_ring(
-                   riak_core_ring:update_meta(?MODULE,
+        false -> 
+            NR = 
+                riak_core_ring:update_meta(?MODULE,
                                               dict:store(
                                                 local_listeners,
                                                 NewListeners,
                                                 ReplConfig),
-                                              Ring))
+                                              Ring),
+            riak_core_ring_manager:set_my_ring(NR),
+            NR
     end,
+    ensure_listeners(NewRing),
     {ok, State#state{ring=NewRing}}.
             
 
@@ -77,7 +85,6 @@ ensure_repl_config(Ring) ->
     end.
 
 ensure_listeners(Ring) ->    
-    io:format("~p~n", [Ring]),
     {ok, ReplConfig} = riak_core_ring:get_meta(?MODULE, Ring),
     LocalListeners = dict:fetch(local_listeners, ReplConfig),
     ToStart = [{IP, Port} || {Node, {IP, Port}} <- LocalListeners, 
