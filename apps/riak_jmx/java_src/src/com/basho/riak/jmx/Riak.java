3103e925e6dfa41e4df039ee4e9c45e54bf9945e
Andy Gross <andy@basho.com>
2010-06-04 14:10:33 -0400

    no annotations

--- apps/riak_jmx/java_src/src/com/basho/riak/jmx/Riak.java
+++ apps/riak_jmx/java_src/src/com/basho/riak/jmx/Riak.java
@@ -128,292 +128,292 @@ public class Riak implements RiakMBean {
 		}
 	}
 
-	@Override
+	
 	public int getCPUNProcs() {
 		return cpuNProcs;
 	}
 
-	@Override
+	
 	synchronized public void setCPUNProcs(int nprocs) {
 		this.cpuNProcs = nprocs;
 	}	
 	
-	@Override
+	
 	public float getMemAllocated() {
 		return memAllocated;
 	}
 
-	@Override
+	
 	synchronized public void setMemAllocated(float memAllocated) {
 		this.memAllocated = memAllocated;
 	}
 
-	@Override
+	
 	synchronized public void setNodeGets(int nodeGets) {
 		this.nodeGets = nodeGets;
 	}
 
-	@Override
+	
 	public int getNodeGets() {
 		return nodeGets;
 	}
 
-	@Override
+	
 	synchronized public void setNodeGetsTotal(int nodeGetsTotal) {
 		this.nodeGetsTotal = nodeGetsTotal;
 	}
 
-	@Override
+	
 	public int getNodeGetsTotal() {
 		return nodeGetsTotal;
 	}
 
-	@Override
+	
 	synchronized public void setNodePuts(int nodePuts) {
 		this.nodePuts = nodePuts;
 	}
 
-	@Override
+	
 	public int getNodePuts() {
 		return nodePuts;
 	}
 
-	@Override
+	
 	synchronized public void setNodePutsTotal(int nodePutsTotal) {
 		this.nodePutsTotal = nodePutsTotal;
 	}
 
-	@Override
+	
 	public int getNodePutsTotal() {
 		return nodePutsTotal;
 	}
 
-	@Override
+	
 	synchronized public void setMemTotal(float memTotal) {
 		this.memTotal = memTotal;
 	}
 
-	@Override
+	
 	public float getMemTotal() {
 		return memTotal;
 	}
 	
-	@Override
+	
 	public int getVnodeGets() {
 		return vnodeGets;
 	}
 
-	@Override
+	
 	synchronized public void setVnodeGets(int vnodeGets) {
 		this.vnodeGets = vnodeGets;
 	}
 
-	@Override
+	
 	public int getVnodeGetsTotal() {
 		return vnodeGetsTotal;
 	}
 
-	@Override
+	
 	synchronized public void setVnodeGetsTotal(int vnodeGetsTotal) {
 		this.vnodeGetsTotal = vnodeGetsTotal;
 	}
 
-	@Override
+	
 	public int getVnodePuts() {
 		return vnodePuts;
 	}
 
-	@Override
+	
 	synchronized public void setVnodePuts(int vnodePuts) {
 		this.vnodePuts = vnodePuts;
 	}
 
-	@Override
+	
 	public int getVnodePutsTotal() {
 		return vnodePutsTotal;
 	}
 
-	@Override
+	
 	synchronized public void setVnodePutsTotal(int vnodePutsTotal) {
 		this.vnodePutsTotal = vnodePutsTotal;
 	}
 
-	@Override
+	
 	public int getPbcActive() {
 		return pbcActive;
 	}
 
-	@Override
+	
 	synchronized public void setPbcActive(int pbcActive) {
 		this.pbcActive = pbcActive;
 	}
 
-	@Override
+	
 	public int getPbcConnects() {
 		return pbcConnects;
 	}
 
-	@Override
+	
 	synchronized public void setPbcConnects(int pbcConnects) {
 		this.pbcConnects = pbcConnects;
 	}
 
-	@Override
+	
 	public int getPbcConnectsTotal() {
 		return pbcConnectsTotal;
 	}
 
-	@Override
+	
 	synchronized public void setPbcConnectsTotal(int pbcConnectsTotal) {
 		this.pbcConnectsTotal = pbcConnectsTotal;
 	}
 
-	@Override
+	
 	public String getNodeName() {
 		return nodeName;
 	}
 
-	@Override
+	
 	synchronized public void setNodeName(String nodeName) {
 		this.nodeName = nodeName;
 	}
 
-	@Override
+	
 	public int getRingCreationSize() {
 		return ringCreationSize;
 	}
 
-	@Override
+	
 	synchronized public void setRingCreationSize(int ringCreationSize) {
 		this.ringCreationSize = ringCreationSize;
 	}
 	
-	@Override
+	
 	public int getCpuAvg1() {
 		return cpuAvg1;
 	}
 
-	@Override
+	
 	synchronized public void setCpuAvg1(int cpuAvg1) {
 		this.cpuAvg1 = cpuAvg1;
 	}
 
-	@Override
+	
 	public int getCpuAvg5() {
 		return cpuAvg5;
 	}
 
-	@Override
+	
 	synchronized public void setCpuAvg5(int cpuAvg5) {
 		this.cpuAvg5 = cpuAvg5;
 	}
 
-	@Override
+	
 	public int getCpuAvg15() {
 		return cpuAvg15;
 	}
 
-	@Override
+	
 	synchronized public void setCpuAvg15(int cpuAvg15) {
 		this.cpuAvg15 = cpuAvg15;
 	}
 	
-	@Override
+	
 	public float getNodeGetFsmTimeMax() {
 		return nodeGetFsmTimeMax;
 	}
 
-	@Override
+	
 	synchronized public void setNodeGetFsmTimeMax(float nodeGetFsmTimeMax) {
 		this.nodeGetFsmTimeMax = nodeGetFsmTimeMax;
 	}
 
-	@Override
+	
 	public float getNodeGetFsmTimeMean() {
 		return nodeGetFsmTimeMean;
 	}
 
-	@Override
+	
 	synchronized public void setNodeGetFsmTimeMean(float nodeGetFsmTimeMean) {
 		this.nodeGetFsmTimeMean = nodeGetFsmTimeMean;
 	}
 
-	@Override
+	
 	public float getNodeGetFsmTimeMedian() {
 		return nodeGetFsmTimeMedian;
 	}
 
-	@Override
+	
 	synchronized public void setNodeGetFsmTimeMedian(float nodeGetFsmTimeMedian) {
 		this.nodeGetFsmTimeMedian = nodeGetFsmTimeMedian;
 	}
 
-	@Override
+	
 	public float getNodeGetFsmTime95() {
 		return nodeGetFsmTime95;
 	}
 
-	@Override
+	
 	synchronized public void setNodeGetFsmTime95(float nodeGetFsmTime95) {
 		this.nodeGetFsmTime95 = nodeGetFsmTime95;
 	}
 
-	@Override
+	
 	public float getNodeGetFsmTime99() {
 		return nodeGetFsmTime99;
 	}
 	
-	@Override
+	
 	synchronized public void setNodeGetFsmTime99(float nodeGetFsmTime99) {
 		this.nodeGetFsmTime99 = nodeGetFsmTime99;
 	}
 	
-	@Override
+	
 	public float getNodePutFsmTimeMax() {
 		return nodePutFsmTimeMax;
 	}
 
-	@Override
+	
 	synchronized public void setNodePutFsmTimeMax(float nodePutFsmTimeMax) {
 		this.nodePutFsmTimeMax = nodePutFsmTimeMax;
 	}
 
-	@Override
+	
 	public float getNodePutFsmTimeMean() {
 		return nodePutFsmTimeMean;
 	}
 	
-	@Override
+	
 	synchronized public void setNodePutFsmTimeMean(float nodePutFsmTimeMean) {
 		this.nodePutFsmTimeMean = nodePutFsmTimeMean;
 	}
 
-	@Override
+	
 	public float getNodePutFsmTimeMedian() {
 		return nodePutFsmTimeMedian;
 	}
 
-	@Override
+	
 	synchronized public void setNodePutFsmTimeMedian(float nodePutFsmTimeMedian) {
 		this.nodePutFsmTimeMedian = nodePutFsmTimeMedian;
 	}
 
-	@Override
+	
 	public float getNodePutFsmTime95() {
 		return nodePutFsmTime95;
 	}
 
-	@Override
+	
 	synchronized public void setNodePutFsmTime95(float nodePutFsmTime95) {
 		this.nodePutFsmTime95 = nodePutFsmTime95;
 	}
 
-	@Override
+	
 	public float getNodePutFsmTime99() {
 		return nodePutFsmTime99;
 	}
 	
-	@Override
+	
 	synchronized public void setNodePutFsmTime99(float nodePutFsmTime99) {
 		this.nodePutFsmTime99 = nodePutFsmTime99;
 	}
