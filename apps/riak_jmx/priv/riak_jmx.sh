0efe36f735549ec259f57ca83b27b4a5d2715f23
Andy Gross <andy@basho.com>
2010-06-07 19:27:40 -0400

    interim fix for remote jmx monitoring

--- apps/riak_jmx/priv/riak_jmx.sh
+++ apps/riak_jmx/priv/riak_jmx.sh
@@ -3,4 +3,4 @@
 ##
 ## Use exec so that the PID doesn't change when invoked
 ##
-exec java -server -cp riak_jmx.jar com.basho.riak.jmx.Main $1 $2
+exec java -server -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=41123 -cp riak_jmx.jar com.basho.riak.jmx.Main $1 $2
