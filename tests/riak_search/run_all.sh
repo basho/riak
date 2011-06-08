22e9acfddcea3df183f73c94dc9e4e78f23b85cd
rzezeski <rzezeski@gmail.com>
2011-06-08 17:43:02 -0400

    Check for failed test and exit appropriately
    tags: az452

--- tests/riak_search/run_all.sh
+++ tests/riak_search/run_all.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 TEST_DIR=`pwd`
+LOG=$TEST_DIR/test.log
 
 cd ../../rel/riak
 
 for x in $TEST_DIR/*_test;
 do
-    echo $x
-    bin/search-cmd test $x 2>&1 | grep -e FAIL -e ERROR
+    echo RUN: $x
+    if ! bin/search-cmd test $x > $LOG 2>&1
+    then
+        printf "\nFAIL: %s\nCheck $LOG\n\n" $x
+        exit 1
+    fi
 done
